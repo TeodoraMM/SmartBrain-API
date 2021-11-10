eedd2092f9bc4b4a3965992223ac5994f698e6b8
TeodoraMM <teodora_mezei12@yahoo.com>
2021-11-10 10:56:00 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const cors=require('cors');
+const knex= require('knex');
+
+const register=require('./controlers/register');
+const signin=require('./controlers/signin');
+const profile=require('./controlers/profile');
+const image=require('./controlers/image')
+
+const db=knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', // = localhost
+    user : 'postgres',
+    password : '1234',
+    database : 'smartbrain'
+  }
+});
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/',(req,res)=>{res.send(database.users);})
+
+app.post('/signin', (req,res) => { signin.handleSignin(req,res,db,bcrypt)});
+
+app.post('/register',(req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id', (req,res ) => { profile.handleProfileGet(req,res,db)})
+
+app.put('/image', (req,res) => { image.handleImage(req,res,db)});
+
+app.post('/imageurl', (req,res) => { image.handleApiCall(req,res)});
+
+app.listen(3001,()=>{
+	console.log('app is running on port 3001')
+});
