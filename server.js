5835e18cbd906c534e38795172c43d0f4b74e7c2
TeodoraMM <teodora_mezei12@yahoo.com>
2021-11-10 12:04:50 +0200

    update nodemon server.js

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.put('/image', (req,res) => { image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) => { image.handleApiCall(req,res)});
 
 app.listen(process.env.PORT || 3001,()=>{
-	console.log('app is running on port ${process.env.port}')
+	console.log(`app is running on port ${process.env.PORT}`)
 });
