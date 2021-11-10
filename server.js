3e00204cd78d402352d4dc145540a4e8be9966b0
TeodoraMM <teodora_mezei12@yahoo.com>
2021-11-10 12:48:04 +0200

    ssl error maybe

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const signin=require('./controlers/signin');
 const profile=require('./controlers/profile');
 const image=require('./controlers/image')
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 const db=knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1', // = localhost
-    user : 'postgres',
-    password : '1234',
-    database : 'smartbrain'
+    connectionString:process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
